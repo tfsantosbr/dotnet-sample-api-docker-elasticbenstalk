e5b4a451af90dca84f46bf408d93208c25f708bd
Tiago Santos <tfsantosbr@gmail.com>
2024-03-28 13:43:03 -0300

    add company

---
+++ src/SampleApi/Controllers/CompaniesController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleApi.Controllers;
+
+[ApiController]
+[Route("companies")]
+public class CompaniesController : ControllerBase
+{
+    private static readonly List<Company> _companies = [];
+    private readonly ILogger<CompaniesController> _logger;
+
+    public CompaniesController(ILogger<CompaniesController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult GetUsers()
+    {
+        return Ok(_companies);
+    }
+
+    [HttpPost]
+    public IActionResult CreateUser([FromBody]Company newCompany)
+    {
+        if(string.IsNullOrWhiteSpace(newCompany.Name))
+        {
+            throw new Exception("Company name can't be empy");
+        }
+
+        _companies.Add(newCompany);
+
+        _logger.LogInformation("Company created {company}", newCompany);
+
+        return Created(newCompany.Id.ToString(), newCompany);
+    }
+}
+
+public class Company
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Name { get; set; } = string.Empty;
+}
