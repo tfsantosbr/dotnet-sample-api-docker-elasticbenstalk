ec7899b6458d5f9c08a57d3bee25a6b2bb1653fa
Tiago Santos <tfsantosbr@gmail.com>
2024-03-30 14:14:24 -0300

    final update

--- src/SampleApi/Controllers/HomeController.cs
+++ src/SampleApi/Controllers/HomeController.cs
@@ -10,6 +10,6 @@ public class HomeController : ControllerBase
     [HttpGet]
     public IActionResult Index()
     {
-        return Ok("Olá mundo =jj))fff");
+        return Ok("Olá mundo ^^");
     }
 }
